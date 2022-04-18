4b148d70e3acb66d264ad57f2bc4233ead24230a
Wenceslao Paez <wcpaez@gmail.com>
2022-04-17 20:17:31 -0500

    Finis card

--- deck_test.go
+++ deck_test.go
@@ -1,1 +1,32 @@
 package main
+
+import (
+	"os"
+	"testing"
+)
+
+func TestNewDeck(t *testing.T) {
+	d := newDeck()
+
+	if len(d) != 20 {
+		t.Errorf("Expected deck lenght of 16, but got %d", len(d))
+	}
+
+	if d[0] != "One of Espadas" {
+		t.Errorf("Bad first element %v", d[0])
+	}
+}
+
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T) {
+	os.Remove("_decktesting")
+	deck := newDeck()
+	deck.saveToFile("_decktesting")
+
+	loadedDeck := newDeckFromFile("_decktesting")
+
+	if len(loadedDeck) != 20 {
+		t.Errorf("Expected deck lenght of 16, but got %d", len(loadedDeck))
+	}
+
+	os.Remove("_decktesting")
+}
